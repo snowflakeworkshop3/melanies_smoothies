44dbdff4bb732e28982728212bac0e07d65582f7
snowflakeworkshop3 <alorgun04@gmail.com>
2025-06-26 11:55:38 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,8 +31,8 @@ if ingredients_list:
 
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
-        my_insert_stmt = (
-        "insert into smoothies.public.orders(ingredients, name_on_order, order_filled) "
-        "values ('" + INGREDIENTS_STRING + "', '" + name_on_order + "')", TRUE)"
+       my_insert_stmt = (
+        "INSERT INTO smoothies.public.orders (ingredients, name_on_order, ordered) "
+        "VALUES ('" + INGREDIENTS_STRING + "', '" + name_on_order + "', TRUE)")
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
