06f5512abb2fc13c825df86500b66a087b05ad85
snowflakeworkshop3 <alorgun04@gmail.com>
2025-06-26 11:37:16 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,6 @@ if ingredients_list:
     "insert into smoothies.public.orders(ingredients, name_on_order) "
     "values ('" + INGREDIENTS_STRING + "', '" + name_on_order + "')")
     time_to_insert = st.button('Submit Order')
-    if INGREDIENTS_STRING:
+    if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
